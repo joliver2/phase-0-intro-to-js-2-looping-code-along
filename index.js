421ec1bc4c0d7194d7b60a028ac0eca284fb1996
joliver427@icloud.com <joliver427@icloud.com>
2022-12-21 20:08:49 -0700

    commit tree 6a150bf8d43af1b2bf9f267a8d392097bd63fb57

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+function writeCards(names, event) {
+    const cardArray = [];
+    for(let i = 0; i < names.length; i++) {
+        cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardArray;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int)
+        int--
+    }
+}
